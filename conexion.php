7fa573d443940dba3d89394e4d75a47b91eb87c5
Felipe Olivares <felipeandreso494@gmail.com>
2019-06-04 22:07:44 -0400

    add *.php and conexion to database

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+	$BBDD_host="localhost";
+	$BBDD_user="root";
+	$BBDD_pass="";
+	$BBDD_BBDD="sirpon";
+
+
+     $con=mysqli_connect($BBDD_host,$BBDD_user,$BBDD_pass);
+     if(mysqli_connect_errno()){
+		 echo "Error con la conexión...";
+	 }
+		 
+	mysqli_set_charset($con,"UTF8");
+	mysqli_select_db($con,$BBDD_BBDD) or die ("No existe Base de Datos...");
