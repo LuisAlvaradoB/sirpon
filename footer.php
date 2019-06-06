48cdb413053634212a1a9c161d30aebd19d31d7a
Felipe Olivares <felipeandreso494@gmail.com>
2019-06-06 00:00:13 -0400

    edit login, footer, head; add bootstrap-combobox.css, bootstrap-combobox.js

--- footer.php
+++ footer.php
@@ -2,6 +2,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="js/spur.js"></script>
+    <script src="js/bootstrap-combobox.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.combobox').combobox();
+        });
+    </script>
 </body>
 
 </html>
